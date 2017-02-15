21bdd86fb6b6d1451c3b5574ac37a592d8636252
liyuanhong <908963295@qq.com>
2017-02-15 11:20:38 +0800

    no

--- app/config/config.php
+++ app/config/config.php
@@ -8,17 +8,7 @@ class Config{
 }
 
 function getMemberFromIP() {
-        /**
-		$json = file_get_contents("http://check.yixia.com/check/index.php?fn=fetchuserlist");
-        $members = json_decode($json,true);
-        $ip = $_SERVER['REMOTE_ADDR'];
-        foreach ($members as $key => $val) {
-                if ($val['ip'] == $ip) {
-                        return $val['sname'];
-                }
-        }
-        return '';
-		*/
+
 		return hello;
 }
 
